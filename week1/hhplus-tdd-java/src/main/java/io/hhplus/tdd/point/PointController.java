7a66bce97585d3c5d2dacc812aacc68386d21c74
DaehoSon <thseogh135@gmail.com>
2025-05-21 23:20:01 +0900

    feat: 컨트롤러 계층과 서비스 계층 분리

--- week1/hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
+++ week1/hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,7 +1,9 @@
 package io.hhplus.tdd.point;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,13 @@ public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
 
+    private final PointService pointService;
+
+    @Autowired
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
+
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
@@ -19,7 +28,8 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("id: {}", id);
+        return pointService.point(id);
     }
 
     /**
@@ -29,7 +39,8 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        log.info("id: {}", id);
+        return pointService.history(id);
     }
 
     /**
@@ -40,7 +51,8 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("id: {}, amount: {}", id, amount);
+        return pointService.charge(id, amount);
     }
 
     /**
@@ -51,6 +63,7 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("id: {}, amount: {}", id, amount);
+        return pointService.use(id, amount);
     }
 }
---
+++ week1/hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,39 @@
+package io.hhplus.tdd.point;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PointService {
+
+    private static final Logger log = LoggerFactory.getLogger(PointService.class);
+
+    public UserPoint point(long id) {
+        UserPoint userPoint = new UserPoint(0, 0, 0);
+
+        log.info("userPoint: {}", userPoint);
+        return userPoint;
+    }
+
+    public List<PointHistory> history(long id) {
+        List<PointHistory> historyList = List.of();
+        log.info("history의 개수: {}", historyList.size());
+        return historyList;
+    }
+
+    public UserPoint charge(long id, long amount) {
+        UserPoint userPoint = new UserPoint(0, 0, 0);
+        log.info("userPoint: {}", userPoint);
+        return userPoint;
+    }
+
+    public UserPoint use(long id, long amount) {
+        UserPoint userPoint = new UserPoint(0, 0, 0);
+        log.info("userPoint: {}", userPoint);
+        return userPoint;
+    }
+}
