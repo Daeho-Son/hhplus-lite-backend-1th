a391577f9ee8350c02b0d565894f97dee1130b78
Daeho-Son <thseogh135@gmail.com>
2025-05-22 21:40:24 +0900

    test: Long.MAX_VALUE만 요청 받지 않도록 테스트 코드 변경

--- week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,9 +28,11 @@ class PointServiceTest {
     }
 
     @Test
-    void point_존재하지_않는_id를_요청하면_500_에러가_발생한다() {
+    void point_존재하지_않는_id를_요청하면_500_에러가_발생한다_1() {
         // given
-        long userId = Long.MAX_VALUE;
+        Random random = new Random();
+        long randomOffset = random.nextLong(1000); // 0부터 999 사이의 랜덤 값
+        long userId = Long.MAX_VALUE - randomOffset;
 
         // when & then
         assertThrows(Exception.class, () -> pointService.point(userId));
