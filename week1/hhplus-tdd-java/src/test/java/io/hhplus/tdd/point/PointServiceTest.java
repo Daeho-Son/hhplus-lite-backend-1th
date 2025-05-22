d473d5ecc28fa4ecd09ba56e4489267f46f378e5
Daeho-Son <thseogh135@gmail.com>
2025-05-22 22:12:26 +0900

    test: PointService에 의존성이 추가됨에 따라 의존성 모킹

--- week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,40 +1,62 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceTest {
+
+    @Mock
+    private UserPointTable userPointTable;
 
     @InjectMocks
     private PointService pointService;
 
     @Test
-    void point_id_1을_요청하면_id_1의_포인트_정보를_반환한다() throws Exception {
+    void point_id_1을_요청하면_id_1의_포인트_정보를_반환한다() {
         // given
         long userId = 1L;
+        long expectedPoint = 100L;
+        long currentTimeMillis = System.currentTimeMillis();
+
+        UserPoint expectedUserPoint = new UserPoint(userId, expectedPoint, currentTimeMillis);
+        when(userPointTable.selectById(userId)).thenReturn(expectedUserPoint);
 
         // when
         UserPoint userPoint = pointService.point(userId);
 
         //then
         assertEquals(userId, userPoint.id());
+        assertEquals(expectedPoint, userPoint.point());
+        assertEquals(currentTimeMillis, userPoint.updateMillis());
     }
 
     @Test
-    void point_존재하지_않는_id를_요청하면_500_에러가_발생한다_1() {
+    void point_존재하지_않는_id를_요청하면_Userpoint_empty를_반환한다() {
         // given
         Random random = new Random();
-        long randomOffset = random.nextLong(1000); // 0부터 999 사이의 랜덤 값
+        long randomOffset = random.nextLong(1000);
         long userId = Long.MAX_VALUE - randomOffset;
+        long currentTimeMillis = System.currentTimeMillis();
 
-        // when & then
-        assertThrows(Exception.class, () -> pointService.point(userId));
+        when(userPointTable.selectById(userId)).thenReturn(UserPoint.empty(userId));
+
+        // when
+        UserPoint userPoint = pointService.point(userId);
+
+        // then
+        assertEquals(userId, userPoint.id());
+        assertEquals(0, userPoint.point());
+        assertTrue(currentTimeMillis <= userPoint.updateMillis());
+        assertTrue(userPoint.updateMillis() <= System.currentTimeMillis());
     }
 }
