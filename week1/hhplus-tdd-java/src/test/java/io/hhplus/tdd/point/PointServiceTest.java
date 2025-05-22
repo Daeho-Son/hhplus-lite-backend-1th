c17006a16409c50b510890925c1b6a90e16fc79b
Daeho-Son <thseogh135@gmail.com>
2025-05-22 22:37:06 +0900

    test: 존재하는 id인 경우 내역 리스트를 반환한다

--- week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ week1/hhplus-tdd-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +19,9 @@ class PointServiceTest {
 
     @Mock
     private UserPointTable userPointTable;
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
 
     @InjectMocks
     private PointService pointService;
@@ -64,12 +68,37 @@ class PointServiceTest {
     @Test
     void history_존재하지_않는_id가_요청되면_빈_배열을_반환한다() {
         // given
-        long userId = 1L;
+        Random random = new Random();
+        long randomOffset = random.nextLong(1000);
+        long userId = Long.MAX_VALUE - randomOffset;
+        when(pointHistoryTable.selectAllByUserId(userId)).thenReturn(List.of());
 
         // when
         List<PointHistory> expected = pointService.history(userId);
 
         // then
         assertEquals(0, expected.size());
+    }
+
+
+    // 의미있는 테스트 코드인가?
+    @Test
+    void history_존재하는_id가_요청되면_리스트를_반환한다() {
+        // given
+        long userId = 1L;
+        List<PointHistory> expected = List.of(
+            new PointHistory(1L, 1L, 1L, TransactionType.CHARGE, 1L),
+            new PointHistory(2L, 2L, 2L, TransactionType.USE, 2L),
+            new PointHistory(3L, 3L, 3L, TransactionType.CHARGE, 3L)
+        );
+
+        when(pointHistoryTable.selectAllByUserId(userId)).thenReturn(expected);
+
+        // when
+        List<PointHistory> actual = pointService.history(userId);
+
+        // then
+        verify(pointHistoryTable, times(1)).selectAllByUserId(userId);
+        assertEquals(expected.size(), actual.size());
     }
 }
